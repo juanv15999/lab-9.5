ff78f0026535745b715e21fe03b0567d98e657a8
juanv15999 <128052220+juanv15999@users.noreply.github.com>
2023-04-11 08:30:27 -0500

    3

---
+++ script.js
@@ -0,0 +1,18 @@
+const miFuncion = async()=> {
+    console.log("x1");
+    const response = await fetch("https://raw.githubusercontent.com/JonatanOrdonez/fetch-requests/main/tienda.json")
+    const data = await response.json();
+
+    const jsonString = JSON.stringify(data);
+    const jasonJSON = JSON.parse(jsonString);
+
+    console.log(jsonString);
+
+    console.log(jasonJSON);
+
+    console.log("x2");
+
+
+
+}
+miFuncion();
